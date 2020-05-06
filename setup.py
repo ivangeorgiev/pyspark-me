d987bf8763fad74936e47771f48f185ef36d9a65
Ivan Georgiev <ivan.georgiev@nl.abnamro.com>
2020-05-06 12:00:17 +0200

    version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyspark-me", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Ivan Georgiev",
     #author_email="ivan.georgiev",
     description="Pyspark tools for everyday use",
