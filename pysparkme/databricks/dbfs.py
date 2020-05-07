8f5c2ef39f4f51a100e666e1840ad5d4a4e0caac
Ivan Georgiev <ivan.georgiev@gmail.com>
2020-05-07 17:52:10 +0200

    adding databricks cli

--- pysparkme/databricks/dbfs.py
+++ pysparkme/databricks/dbfs.py
@@ -1,25 +1,35 @@
 import base64
+from dataclasses import dataclass, field
+from typing import List
 from .common import Api, DatabricksLinkException, ERR_RESOURCE_DOES_NOT_EXIST
 from .common import bite_size_str
+
+@dataclass
+class DBFS_FileInfo:
+    path: str
+    is_dir: bool
+    file_size: int
+    human_size: str = field(init=False, compare=False)
+
+    def __post_init__(self):
+        self.human_size = bite_size_str(self.file_size)
 
 class DBFS(Api):
     def __init__(self, link):
         super().__init__(link, path='dbfs')
 
-    def list(self, path=None, humanize=False):
+    def list(self, path=None) -> List[DBFS_FileInfo]:
         get_result = self.link.get(
             self.path('list'),
             params=dict(path=(path or '/')))
         files = get_result.get('files', [])
-        if (humanize):
-            for f in files:
-                f['bite_size'] = bite_size_str(f['file_size'])
-        return files
+        result = [DBFS_FileInfo(**f) for f in files]
+        return result
 
-    def ls(self, path=None, humanize=False):
+    def ls(self, path=None) -> List[DBFS_FileInfo]:
         return self.list(path, humanize)
 
-    def exists(self, path):
+    def exists(self, path) -> bool:
         try:
             self.list(path)
             result = True
